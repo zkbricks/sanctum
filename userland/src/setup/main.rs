060d9b5b5762e5312afed78c6dab93cc8727af7e
Rohit Sinha <rohit@zkbricks.com>
2024-04-05 09:52:13 -0400

    setup for merk;e update circuit

--- userland/src/setup/main.rs
+++ userland/src/setup/main.rs
@@ -7,11 +7,30 @@ async fn main() -> reqwest::Result<()> {
 
     println!("initiating circuit setup for onramp circuit...");
     let (onramp_pk, onramp_vk) = onramp_circuit::circuit_setup();
-    utils::write_groth_key_to_file(&onramp_pk, "/tmp/sanctum/onramp.pk", &onramp_vk, "/tmp/sanctum/onramp.vk");
+    utils::write_groth_key_to_file(
+        &onramp_pk,
+        "/tmp/sanctum/onramp.pk",
+        &onramp_vk,
+        "/tmp/sanctum/onramp.vk"
+    );
 
     println!("initiating circuit setup for payment circuit...");
     let (payment_pk, payment_vk) = payment_circuit::circuit_setup();
-    utils::write_groth_key_to_file(&payment_pk, "/tmp/sanctum/payment.pk", &payment_vk, "/tmp/sanctum/payment.vk");
+    utils::write_groth_key_to_file(
+        &payment_pk,
+        "/tmp/sanctum/payment.pk",
+        &payment_vk,
+        "/tmp/sanctum/payment.vk"
+    );
+
+    println!("initiating circuit setup for merkle update circuit...");
+    let (merkle_update_pk, merkle_update_vk) = lib_sanctum::merkle_update_circuit::circuit_setup();
+    utils::write_groth_key_to_file(
+        &merkle_update_pk,
+        "/tmp/sanctum/merkle_update.pk",
+        &merkle_update_vk,
+        "/tmp/sanctum/merkle_update.vk"
+    );
 
     println!("completed trusted setup...");
 
